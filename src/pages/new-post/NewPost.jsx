a6c0bc387f0396272d14bd2be508a01af7eb63c9
Ki-fi <k.meekels@gmail.com>
2025-06-14 14:28:15 +0200

    Opzet en styling formulier

--- src/pages/new-post/NewPost.jsx
+++ src/pages/new-post/NewPost.jsx
@@ -1,9 +1,70 @@
-
+import Input from "../../components/input/Input.jsx";
+import {useState} from "react";
+import './NewPost.css';
 
 function NewPost() {
+
+    const [formState, setFormState] = useState({
+        title: '',
+        subtitle: '',
+        author: '',
+        content: '',
+    })
+
+    function handleChange(e) {
+        const changedField = e.target.name;
+        const newValue = e.target.value;
+
+        setFormState({
+            ...formState,
+            [changedField]: newValue,
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <h1>page</h1>
+        <>
+            <form onSubmit={handleSubmit}>
+                <h1>Post toevoegen</h1>
+                <Input
+                    labelName={"Titel"}
+                    name={"title"}
+                    value={formState.title}
+                    onChange={handleChange}
+                />
+                <Input
+                    labelName={"Ondertitel"}
+                    name={"subtitle"}
+                    value={formState.subtitle}
+                    onChange={handleChange}
+                />
+                <Input
+                    labelName={"Voor- en achternaam"}
+                    name={"author"}
+                    defaultValue={0}
+                    value={formState.author}
+                    onChange={handleChange}
+                />
+                <label className="textarea-label" htmlFor="textarea"> Blogpost
+                    <textarea
+                        name="Blogpost"
+                        cols="20" rows="7"
+                        value={formState.content}
+                        onChange={handleChange}
+                    ></textarea>
+                </label>
+                <button type="submit">
+                    Toevoegen
+                </button>
+            </form>
+        </>
     )
 }
 
 export default NewPost;
+
+
+
