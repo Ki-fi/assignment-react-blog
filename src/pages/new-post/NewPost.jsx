24ad70530bca1aecefa3a1a2636462dd2fe2ffb5
Ki-fi <k.meekels@gmail.com>
2025-06-16 11:31:06 +0200

    alert en form validation toegevoegd

--- src/pages/new-post/NewPost.jsx
+++ src/pages/new-post/NewPost.jsx
@@ -1,9 +1,14 @@
 import Input from "../../components/input/Input.jsx";
 import {useState} from "react";
 import './NewPost.css';
+import enforceRequiredField from "../../helpers/enforceRequiredField.js";
+import {useNavigate} from "react-router-dom";
+import Alert from "../../components/alert/Alert.jsx";
 
 function NewPost() {
 
+    const navigate = useNavigate();
+    const [alertMessage, setAlertMessage] = useState('');
     const [formState, setFormState] = useState({
         title: '',
         subtitle: '',
@@ -23,12 +28,30 @@ function NewPost() {
 
     function handleSubmit(event) {
         event.preventDefault();
+        const isValid = enforceRequiredField(formState);
+        const currentDate = new Date().toISOString()
+
+        if (isValid) {
+            setAlertMessage(isValid)
+            return;
+        }
+
+        setAlertMessage("");
+
+        console.log(`
+                Titel: ${formState.title}
+                Ondertitel: ${formState.subtitle}
+                Content: ${formState.content}
+                Auteur: ${formState.author}
+                Datum: ${currentDate}`)
+        navigate("/posts");
     }
 
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <h1>Post toevoegen</h1>
+                {alertMessage && <Alert message={alertMessage} />}
                 <Input
                     labelName={"Titel"}
                     name={"title"}
@@ -50,7 +73,7 @@ function NewPost() {
                 />
                 <label className="textarea-label" htmlFor="textarea"> Blogpost
                     <textarea
-                        name="Blogpost"
+                        name="content"
                         cols="20" rows="7"
                         value={formState.content}
                         onChange={handleChange}
